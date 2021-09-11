018681d7d791de6740dd63ade2e1c7abe8e0a054
Mahbubur Rahman <rifat0153@gmail.com>
2021-09-11 16:05:51 +0600

    Code clean up

--- lib/features/product/services/product_service.dart
+++ lib/features/product/services/product_service.dart
@@ -10,43 +10,12 @@ abstract class BaseProductService {
 
 class ProductService extends GetConnect implements BaseProductService {
   @override
-  void onInit() {
-    super.onInit();
-
-    httpClient.baseUrl = 'http://3.1.180.10/api/';
-  }
-
-  @override
   Future<ProductState> getProduct({required String url}) async {
-    final client = http.Client();
-
-    // try {
-    //   final http.Response response = await client.get(
-    //     Uri.parse(url),
-    //     headers: {"Content-Type": "application/json"},
-    //   ).timeout(const Duration(seconds: 5));
-
-    //   final Map<String, dynamic> body = jsonDecode(response.body) as Map<String, dynamic>;
-
-    //   print(body);
-
-    //   final Product product = Product.fromJson(body);
-
-    //   return ProductState.data(product);
-    // } catch (e) {
-    //   print(e);
-    //   return ProductState.error(e);
-    // } finally {
-    //   client.close();
-    // }
-
     try {
       final productResponse = await get(
         url,
         decoder: (data) => Product.fromJson(data as Map<String, dynamic>),
       );
-
-      print(productResponse.body);
 
       if (productResponse.body != null) {
         return ProductState.data(productResponse.body!);
@@ -54,8 +23,6 @@ class ProductService extends GetConnect implements BaseProductService {
         throw Error('Product Not Loaded');
       }
     } catch (e, s) {
-      print(e);
-      print(s);
       return ProductState.error(e);
     }
   }

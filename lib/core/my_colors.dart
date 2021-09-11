7409761e197f981de56e8e2811b44950f01cabc5
Mahbubur Rahman <rifat0153@gmail.com>
2021-09-11 15:27:08 +0600

    BG COLOR CHANGED

--- lib/core/my_colors.dart
+++ lib/core/my_colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class MyColors {
   static const brand_color = Color(0xFFDD3935);
   static const dark_2 = Color(0xFF616161);
-  static const bg_product = Color(0xffE5E5E5);
+  static const bg_product = Color(0xffeef3f9);
   static const btn_yellow = Color(0xffF2C94C);
 }
